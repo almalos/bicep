6fdc7a009a9f0fa871051799331fadf5568c0986
almalos <almalos@almalos.com>
2023-03-30 15:40:35 +0200

    buildCapture path

--- aib/scripts/buildCapture.ps1
+++ aib/scripts/buildCapture.ps1
@@ -3,6 +3,8 @@ $resourceGroupName = "rg-avdimage"
 $imageTemplateName = "avd10ImageTemplate01"
 
 #Build image
+Install-Module  az.imagebuilder -Force
+Import-Module az.imagebuilder
 Start-AzImageBuilderTemplate -ResourceGroupName $resourceGroupName -Name $imageTemplateName -NoWait
 
 #Get build status
